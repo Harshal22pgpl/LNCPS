1e560386ebba626ef515990b068c06bc4acbdf96
Vedant06kumar <152692000+Vedant06kumar@users.noreply.github.com>
2024-09-09 16:45:18 +0530

    new changes

--- app/institute/fc/page.js
+++ app/institute/fc/page.js
@@ -4,20 +4,20 @@ import SliderMain from "@/app/components/SliderMain/SliderMain";
 import React from "react";
 
 export default function page() {
-  const images = ["/baggi.jpg", "/mainImg.jpeg", "/baggi.jpg"];
+  const images = ["/ar-3.jpg",];
   return (
     <div>
-      <SliderMain images={images} heading="Finance Commitee" />
+      <SliderMain images={images} heading="Anti-Ragging" />
       <div className="w-full mx-auto flex relative mt-2  font-semibold text-lg">
         
         <div className="w-2/3 px-20 ">
-          <h1 className="my-1 p-2">Finance Commitee (Members)</h1>
+          <h1 className="my-1 p-2">Anti-Ragging Commitee (Members)</h1>
           <GovernorCard />
           <GovernorCard />
           <GovernorCard />
 
           <GovernorCard />
-          <h1 className="my-1 p-2">Finance Commitee (Secretary)</h1>
+          <h1 className="my-1 p-2">Anti-Ragging (Secretary)</h1>
           <GovernorCard />
         </div>
         <div className="w-1/3 h-[600px] sticky top-20 left-0 py-10 ">
