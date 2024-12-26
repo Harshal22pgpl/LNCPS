86c119c3d46ac1f416cb4e70826d936943f18bed
Vedant06kumar <152692000+Vedant06kumar@users.noreply.github.com>
2024-12-27 00:28:29 +0530

    new updates

--- app/components/AbouttheInstitute/AbouttheInstitute.jsx
+++ app/components/AbouttheInstitute/AbouttheInstitute.jsx
@@ -13,7 +13,7 @@ export default function AbouttheInstitute() {
 Lakshmi Narain College of Professional Studies (LNCP) is a premier institution dedicated to providing high-quality education and skill development to students across various professional disciplines. Located in the vibrant educational landscape of India, LNCP is committed to nurturing future leaders and innovators with a strong emphasis on practical knowledge, ethical values, and global perspectives. The institute offers a wide range of undergraduate and postgraduate programs designed to equip students with the knowledge and skills needed to excel in today’s dynamic professional world
           </p>
           <p className="  lg:px-10 lg:pb-5 border-b p-3 ">
-          The institute also emphasizes research, innovation, and skill development, supported by state-of-the-art infrastructure and modern facilities. LNCP's dynamic campus life, with various clubs, cultural activities, and technical events, enriches the student experience and prepares graduates to excel as professionals and leaders in their chosen fields.
+          The institute also emphasizes research, innovation, and skill development, supported by state-of-the-art infrastructure and modern facilities. LNCP is dynamic campus life, with various clubs, cultural activities, and technical events, enriches the student experience and prepares graduates to excel as professionals and leaders in their chosen fields.
           </p>
           <div className="flex lg:flex-row flex-col mt-3 lg:mt-0 ">
             <div className=" lg:p-10 p-3  flex items-start">
